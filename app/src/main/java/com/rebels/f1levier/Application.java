2bfc32d09b316aaee1b2fb34ba1a9d2a8d667f5d
Serge Bissari <dbissari@gmail.com>
2018-10-05 23:18:50 +0200

    Added and initialized Realm - Added the gradle dependency in project - Applied the gradle dependency in app - Created a main application class then initialized Realm

---
+++ app/src/main/java/com/rebels/f1levier/Application.java
@@ -0,0 +1,14 @@
+package com.rebels.f1levier;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+        RealmConfiguration config = new RealmConfiguration.Builder().build();
+        Realm.setDefaultConfiguration(config);
+    }
+}
